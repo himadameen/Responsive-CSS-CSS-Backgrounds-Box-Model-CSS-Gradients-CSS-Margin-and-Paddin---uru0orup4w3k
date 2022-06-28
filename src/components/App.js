24942f8dda1fa36ed004f1f623a085967354ba19
himmad ameen <ameenhh1620@gmail.com>
2022-06-28 18:08:32 +0530

    my changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,13 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
-  return <div id="main"></div>;
+  return (
+  <>
+    <div id="main">
+      <div>hello</div>
+    </div>
+  </>
+  )
 };
 
 export default App;
